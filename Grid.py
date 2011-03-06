94ccb8d6a9bfb60b7e43254ef8fe6d0f2eca369f
sayatetsu <sayatetsu@0445f67c-1c08-0bea-fbdb-400c37246c67>
2011-03-06 22:50:35 +0000

    - added mesh landscape to midpoint.py - terrain for simulation is not yet working!

--- Grid.py
+++ Grid.py
@@ -15,5 +15,5 @@ class grid:
                 c = visual.color.gray(0.65)
             else:
                 c = visual.color.gray(0.25)
-            visual.convex(frame=self.frame, pos=[(i,0,size),(i,0,-size)], color=c)
-            visual.convex(frame=self.frame, pos=[(size,0,i),(-size,0,i)], color=c)
+            visual.curve(frame=self.frame, pos=[(i,0,size),(i,0,-size)], color=c)
+            visual.curve(frame=self.frame, pos=[(size,0,i),(-size,0,i)], color=c)
